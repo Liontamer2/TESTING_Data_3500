cfc0abb3557003840ea8aeb04de75d1cce262967
Liontamer2 <93547791+Liontamer2@users.noreply.github.com>
2026-01-28 21:15:07 +0000

    Edits to my lecture code

--- lecture code/Week4(DM).py
+++ lecture code/Week4(DM).py
@@ -64,20 +64,95 @@ number = random.randint(1,100)
 5. change the secrect number to random number
 """
 
-guess = 0
-solved = True
-print("Welcome to the Guessing Game!")
+#guess = 0
+#solved = True
+#print("Welcome to the Guessing Game!")
 
-while solved == True:
-    try:
-        guess = int(input("Guess the secret number: "))
-        if guess < number:
-            print("Your guess is low. Guess Again")
-        elif guess > number:
-            print("Your guess is high. Guess again")
-        else:
-            print("You got it correct!")
-            solved == False
-            break  
-    except ValueError:
-        print("Invalid input. Please enter a whole number.")
+#while solved == True:
+#    try:
+#        guess = int(input("Guess the secret number: "))
+#        if guess < number:
+#            print("Your guess is low. Guess Again")
+#        elif guess > number:
+#            print("Your guess is high. Guess again")
+#        else:
+#            print("You got it correct!")
+#            solved == False
+#            break  
+#    except ValueError:
+#        print("Invalid input. Please enter a whole number.")
+
+"""
+This problem was asked by Google
+
+Given a list of numbers, and a number 'k', return whether any two numbers from the list add up to 'k' 
+
+For example: [10, 15, 3, 7], k = 17 print out 'yes' or 'true' since 10 + 7 = 17 
+"""
+
+
+# augmented assignment 
+age = 16
+age = age + 1
+print("age:", age)
+age += 1
+print("age:", age)
+age -= 1 
+print("age:", age)
+age *= 1
+print("age:", age)
+age /= 1
+print("age:", age)
+age %= 1 
+print("age:", age)
+age //= 1
+print("age:", age) 
+age **= 1
+print("age:", age)
+
+# for loops
+
+name = "Clayton Stoddard"
+
+for letter in name:
+    print("letter:", letter)
+
+for i in range(10):
+    print("i:", i)
+
+names = ["Alexandra", "Lily", "Ronald"]
+
+for name in names: 
+    print("Name:", names)
+    for character in names: 
+        print("Character:", character)
+
+# guess the secret number
+# if the user gets to high, tell them and let the keep guessing 
+# if to low, tell them and let them keep guessing
+# if correct, print out that they are correct  
+
+import random
+secret_num = random.randint(1,100)
+
+for i in range(10):
+    guess = int(input('Enter your guess: '))
+    if guess < number:
+        print("Your guess is low. Guess Again")
+    elif guess > number:
+      print("Your guess is high. Guess again")
+    else:
+        print("You got it correct!")
+        break 
+
+# count all even number in a range of 1 to 20 
+
+
+counter = 0 
+for i in range(2, 21, 2):
+#    if i % 2 == 0: 
+#        counter += 1 
+    print("i:", i)
+
+# print("There are", counter, "even numbers between 1 and 20")
+
